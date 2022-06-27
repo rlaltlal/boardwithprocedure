6cc3d4a245434c74c831633c1a5218c8f76602d9
jinho yang <jinho6445@gmail.com>
2022-06-27 11:50:04 +0900

    d

--- BoardsP/src/main/java/com/green/pdssp/service/typeHandler/ListMapHandler.java
+++ BoardsP/src/main/java/com/green/pdssp/service/typeHandler/ListMapHandler.java
@@ -11,6 +11,7 @@ import org.apache.ibatis.type.JdbcType;
 import org.apache.ibatis.type.TypeHandler;
 //import org.springframework.jdbc.support.nativejdbc.CommonsDbcpNativeJdbcExtractor;
 
+import oracle.jdbc.OracleConnection;
 import oracle.sql.ARRAY;
 import oracle.sql.ArrayDescriptor;
 
@@ -28,48 +29,41 @@ public class ListMapHandler implements TypeHandler<Object> {
    @Override
    public void setParameter(PreparedStatement ps, int i, Object parameter, JdbcType jdbcType) throws SQLException {
   
-	   //Spring 4,5  
-	   
-	   // https://stackoverflow.com/questions/65263907/mybatis-passing-string-to-oracle-stored-procedure 
-		 
-//	    OracleConnection conn = ps.getConnection().unwrap(OracleConnection.class);
-//	    Array array = conn.createOracleArray("FILE_ARRAY", parameter);
-//	    ps.setArray(i, array);  
-
-	   
-//	   Spring old 버전 
-//        List<String>  list = (List<String>) parameter;
-//      
-//      // db연결
-//      CommonsDbcpNativeJdbcExtractor extractor
-//        = new CommonsDbcpNativeJdbcExtractor();
-//      Connection  conn = extractor.getNativeConnection(
-//         ps.getConnection()   );
-//      
-//      ArrayDescriptor  desc
-//        = ArrayDescriptor.createDescriptor("FILE_ARRAY", conn);
-//      
-//      // 넘어온 파일명이 하나도 없을때
-//      //  i 번째 파라미터 배열 방안에 null 값을 지정
-//      if(list == null || list.size() == 0) {
-//         
-//         ps.setArray( i,
-//            new ARRAY(desc, conn, new String[] { null } ) );
-//         
-//         return;
-//      }
-//         
-//      // 넘어온 파일목록 처리
-//      // in_filename(1) = list.get(j)
-//      String []   strings  =  new String[list.size()];
-//      for (int j = 0; j < list.size(); j++) {
-//         strings[j]  = list.get(j);
-//      }
-//      
-//      parameter  = new ARRAY(desc, conn, strings);
-//      
-//      ps.setArray(i, ( oracle.sql.ARRAY )parameter );
-      
+	   List<String>  list = (List<String>) parameter;
+	      
+	      // db연결
+	      /* spring 4 + ojdbc6
+	      CommonsDbcpNativeJdbcExtractor extractor
+	        = new CommonsDbcpNativeJdbcExtractor();
+	      Connection  conn = extractor.getNativeConnection(
+	         ps.getConnection()   );
+	      */
+	      // spring 5 + ojdbc11
+	       OracleConnection conn = ps.getConnection().unwrap(OracleConnection.class);   
+	      
+	      ArrayDescriptor  desc
+	        = ArrayDescriptor.createDescriptor("FILE_ARRAY", conn);
+	      
+	      // 남어온 파일명이 하나도 없을때
+	      //  i 번째 파라미터 배열 방안에 null 값을 지정
+	      if(list == null || list.size() == 0) {
+	         
+	         ps.setArray( i,
+	            new ARRAY(desc, conn, new String[] { null } ) );
+	         
+	         return;
+	      }
+	         
+	      // 넘어온 파일목록 처리
+	      // in_filename(1) = list.get(j)
+	      String []   strings  =  new String[list.size()];
+	      for (int j = 0; j < list.size(); j++) {
+	         strings[j]  = list.get(j);
+	      }
+	      
+	      parameter  = new ARRAY(desc, conn, strings);
+	      
+	      ps.setArray(i, ( oracle.sql.ARRAY )parameter );
    }
 
    @Override
